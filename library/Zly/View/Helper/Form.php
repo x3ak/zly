2188f98e008af26a74d719be404ed43892cb7904
Evgheni.Poleacov <evgheni.poleacov@gmail.com>
2011-07-25 14:42:00 +0300

    clean up library

--- library/Zly/View/Helper/Form.php
+++ library/Zly/View/Helper/Form.php
@@ -1,6 +1,7 @@
 <?php
+namespace Zly\View\Helper;
 
-class Zly_View_Helper_Form extends Zend_View_Helper_FormElement
+class Form extends \Zend\View\Helper\FormElement
 {
 
     protected $_additionalContent = array();
@@ -48,7 +49,7 @@ class Zly_View_Helper_Form extends Zend_View_Helper_FormElement
      *
      * @param string $field
      * @param string $value
-     * @return Zly_View_Helper_Form
+     * @return \Zly\View\Helper\Form
      */
     public function setMarker($field, $value)
     {
@@ -61,7 +62,7 @@ class Zly_View_Helper_Form extends Zend_View_Helper_FormElement
      * Add current form marker to already exist markers
      * @param string $field
      * @param string $value
-     * @return Zly_View_Helper_Form
+     * @return \Zly\View\Helper\Form
      */
     public function addMarker($field, $value)
     {
@@ -71,7 +72,7 @@ class Zly_View_Helper_Form extends Zend_View_Helper_FormElement
 
     /**
      * Remove all current form markers
-     * @return Zly_View_Helper_Form
+     * @return \Zly\View\Helper\Form
      */
     public function clearMarkers()
     {
